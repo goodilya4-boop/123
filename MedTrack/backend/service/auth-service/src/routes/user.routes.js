6fb7a68a94d60c9df032a56542a3a98428f84143
goodilya4-boop <goodilya4@gmail.com>
2026-09-24 15:22:18 +0500

    security(auth): harden authentication service

--- MedTrack/backend/service/auth-service/src/routes/user.routes.js
+++ MedTrack/backend/service/auth-service/src/routes/user.routes.js
@@ -5,16 +5,13 @@ module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
             "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
+            "Authorization, x-access-token, Origin, Content-Type, Accept"
         );
         next();
     });
 
     app.get("/api/users", [authJwt.verifyToken, authJwt.isAdmin], controller.findAll);
-
     app.get("/api/users/:id", [authJwt.verifyToken], controller.findOne);
-
     app.put("/api/users/:id", [authJwt.verifyToken], controller.update);
-
     app.delete("/api/users/:id", [authJwt.verifyToken, authJwt.isAdmin], controller.delete);
 };
