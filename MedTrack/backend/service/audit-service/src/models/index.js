33416e954dc3e5eb5ec0ec15f82125560262a643
goodilya4-boop <goodilya4@gmail.com>
2026-09-24 15:49:50 +0500

    feat(audit): implement audit microservice

--- MedTrack/backend/service/audit-service/src/models/index.js
+++ MedTrack/backend/service/audit-service/src/models/index.js
@@ -3,9 +3,10 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
     host: config.HOST,
+    port: config.PORT,
     dialect: config.dialect,
     pool: config.pool,
-    define: { schema: 'MedTrack' },
+    define: { schema: "MedTrack" },
     logging: false
 });
 
@@ -13,16 +14,16 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.audit  = require("./audit.model.js")(sequelize, Sequelize);
+db.audit = require("./audit.model.js")(sequelize, Sequelize);
 db.access = require("./access.model.js")(sequelize, Sequelize);
 
-db.access.hasMany(db.audit, { 
-    foreignKey: 'user_access', 
-    as: 'audit_logs' 
+db.access.hasMany(db.audit, {
+    foreignKey: "user_access",
+    as: "audit_logs"
 });
-db.audit.belongsTo(db.access, { 
-    foreignKey: 'user_access', 
-    as: 'access_right' 
+db.audit.belongsTo(db.access, {
+    foreignKey: "user_access",
+    as: "access_right"
 });
 
 module.exports = db;
