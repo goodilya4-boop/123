7a410061b3856a4f5601fe64c0ae5670eee48d42
goodilya4-boop <goodilya4@gmail.com>
2026-09-24 15:22:15 +0500

    security(auth): harden authentication service

--- MedTrack/backend/service/auth-service/src/routes/auth.routes.js
+++ MedTrack/backend/service/auth-service/src/routes/auth.routes.js
@@ -5,17 +5,14 @@ module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
             "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
+            "Authorization, x-access-token, Origin, Content-Type, Accept"
         );
         next();
     });
 
     app.post(
         "/api/auth/signup",
-        [
-            verifySignUp.checkDuplicateEmail,
-            verifySignUp.checkRoleExisted
-        ],
+        [verifySignUp.checkDuplicateEmail],
         controller.signup
     );
 
